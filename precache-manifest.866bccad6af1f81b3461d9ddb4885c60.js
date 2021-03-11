d52611316bc360e71a6ffc592607ef02a44dd1ca
andyukra <andyguachin87@gmail.com>
2021-03-11 17:54:15 -0300

    v 1.0.0

---
+++ precache-manifest.866bccad6af1f81b3461d9ddb4885c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899a8749e5c24a9831a6775100d41bef",
+    "url": "/buscaFotos/index.html"
+  },
+  {
+    "revision": "bc907b723aaf8aaed14a",
+    "url": "/buscaFotos/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "47368d1d727027b30196",
+    "url": "/buscaFotos/static/js/2.a8ca0ba7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buscaFotos/static/js/2.a8ca0ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc907b723aaf8aaed14a",
+    "url": "/buscaFotos/static/js/main.2f50f1dd.chunk.js"
+  },
+  {
+    "revision": "f815be1bd63552585541",
+    "url": "/buscaFotos/static/js/runtime-main.e52ab425.js"
+  }
+]);
